1de767b50a632d0529ae56856bbbed1a8558d81a
Alon Rab <alonrab@outlook.com>
2019-01-05 22:05:40 +0200

    -- --- .-. ... . / -.-. --- -.. . / .. ... / .- .-- ... --- -- .

--- MorseCode.js
+++ MorseCode.js
@@ -14,7 +14,7 @@ class ScoreTree extends BSTree {
         else if (score > this.score && this.rightChild) {
             this.rightChild.insertNode(key, score)
         }
-        else if(score <= this.score && this.leftChild) {
+        else if (score <= this.score && this.leftChild) {
             this.leftChild.insertNode(key, score)
         }
         else if (score <= this.score) {
@@ -24,14 +24,47 @@ class ScoreTree extends BSTree {
             this.rightChild = new ScoreTree(key, score)
         }
     }
-    findLetter () {
-        // this method should record the path to a given letter
+    findLetter(letter) {
+        // this method should record the path to a given letter - right: Dash , left: Dot
+        let score = alphabet[letter]
+        if (score === this.score) {
+            return ` `
+        } else if (this.score < score) {
+            return `-` + this.rightChild.findLetter(letter)
+        } else if (this.score > score) {
+            return `.` + this.leftChild.findLetter(letter)
+        } 
     }
-    translateWord() {
+    translateWord(word) {
         //this method should translate a given word from text to Morse Code
+        let obj = ``
+        while (word !== ``) {
+            let letter = word[0]
+            word = word.slice(1)
+            if (letter === ` `) {
+                obj += `/ `
+            } else if(letter!==undefined){
+                obj += this.findLetter(letter.toUpperCase())
+            }
+        }
+        return obj
     }
-    translateMorse() {
+    translateMorse(sentence, param = this) {
         // this function should translate a given code from Morse to English
+        let mors = sentence[0]
+        sentence = sentence.slice(1)
+        if (mors === undefined) {
+            return this.value
+        } else if (mors === `.`) {
+            return this.leftChild.translateMorse(sentence, param)
+        } else if (mors === `-`) {
+            return this.rightChild.translateMorse(sentence, param)
+        } else if (mors === `/` && sentence[0] === ` `) {
+            sentence = sentence.slice(1)
+            return ` ` + param.translateMorse(sentence, param)
+        } else if (mors === ` `) {
+            return this.value + param.translateMorse(sentence, param)
+        }
     }
 }
 //initializing the MorseCode tree
@@ -40,7 +73,8 @@ Object.keys(alphabet).forEach(l => {
     morseCode.insertNode(l, alphabet[l])
 })
 
-morseCode.translateWord("welcome") // should print .-- . .-.. -.-. --- -- . 
-morseCode.translateWord("elevation is cool") // should print . .-.. . ...- .- - .. --- -. /.. ... /-.-. --- --- .-.. 
-morseCode.translateMorse(".... ---- ....")
-morseCode.translateMorse("-. .. -.-. . / .--- --- -... / --- -. / - .... . / .-.. . ... ... --- -.")
+// console.log(morseCode.translateWord("welcome")) // should print .-- . .-.. -.-. --- -- . 
+// console.log(morseCode.translateWord("elevation is cool")) // should print . .-.. . ...- .- - .. --- -. /.. ... /-.-. --- --- .-.. 
+// console.log(morseCode.translateMorse("... --- ..."))
+// console.log(morseCode.translateMorse("-. .. -.-. . / .--- --- -... / --- -. / - .... . / .-.. . ... ... --- -."))//NICE JOB ON THE LESSON
+console.log(morseCode.translateWord(`morse code is awsome`))
